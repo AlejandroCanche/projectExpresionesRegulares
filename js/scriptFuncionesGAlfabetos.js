56b68b9e245c7442dff23b7f82573d4611667a0d
AlejandroCanche <viajero31469@innovaccion.mx>
2021-01-22 00:09:01 -0600

    Cambios 22/01

---
+++ js/scriptFuncionesGAlfabetos.js
@@ -0,0 +1,27 @@
+function mostrarAB() {
+	document.getElementById('palabrasAceptadasAB').style.display = 'block';
+}
+
+function ocultarAB() {
+	document.getElementById('palabrasAceptadasAB').style.display = 'none';
+}
+
+
+
+function mostrarABC() {
+	document.getElementById('palabrasAceptadasABC').style.display = 'block';
+}
+
+function ocultarABC() {
+	document.getElementById('palabrasAceptadasABC').style.display = 'none';
+}
+
+
+
+function mostrarABCD() {
+	document.getElementById('palabrasAceptadasABCD').style.display = 'block';
+}
+
+function ocultarABCD() {
+	document.getElementById('palabrasAceptadasABCD').style.display = 'none';
+}
---
+++ js/scriptMostrarArticles.js
@@ -0,0 +1,113 @@
+$(document).ready(function(){
+	$('ul.tabs li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.tabs li a').click(function(){
+		$('ul.tabs li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeTab = $(this).attr('href');
+		$(activeTab).show();
+		return false;
+	});
+});
+
+
+$(document).ready(function(){
+	$('ul.buttons li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.buttons li a').click(function(){
+		$('ul.buttons li a').removeClass('active');
+		$(this).addClass('active2');
+		$('.secciones article').hide();
+
+		var activeButton = $(this).attr('href');
+		$(activeButton).show();
+		return false;
+	});
+});
+
+$(document).ready(function(){
+	$('ul.buttonsTeoria li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.buttonsTeoria li a').click(function(){
+		$('ul.buttonsTeoria li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeButton = $(this).attr('href');
+		$(activeButton).show();
+		return false;
+	});
+});
+
+
+$(document).ready(function(){
+	$('ul.buttonsOER li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.buttonsOER li a').click(function(){
+		$('ul.buttonsOER li a').removeClass('active');
+		$(this).addClass('active2');
+		$('.secciones article').hide();
+
+		var activeButton = $(this).attr('href');
+		$(activeButton).show();
+		return false;
+	});
+});
+
+$(document).ready(function(){
+	$('ul.buttonsAnimaciones li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.buttonsAnimaciones li a').click(function(){
+		$('ul.buttonsAnimaciones li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeButton = $(this).attr('href');
+		$(activeButton).show();
+		return false;
+	});
+});
+
+$(document).ready(function(){
+	$('ul.buttonsRegresar li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.buttonsRegresar li a').click(function(){
+		$('ul.buttonsRegresar li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeButton = $(this).attr('href');
+		$(activeButton).show();
+		return false;
+	});
+});
+
+$(document).ready(function(){
+	$('ul.buttonGALF li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.buttonGALF  li a').click(function(){
+		$('ul.buttonGALF li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeButton = $(this).attr('href');
+		$(activeButton).show();
+		return false;
+	});
+});
