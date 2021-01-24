7a60d78f424bf6ca7e2e1deb44e7184a6754137a
AlejandroCanche <viajero31469@innovaccion.mx>
2021-01-24 11:23:44 -0600

    Cambios 24/01

--- js/scriptMostrarArticles.js
+++ js/scriptMostrarArticles.js
@@ -111,3 +111,19 @@ $(document).ready(function(){
 		return false;
 	});
 });
+
+$(document).ready(function(){
+	$('ul.buttonsEjER li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.buttonsEjER  li a').click(function(){
+		$('ul.buttonsEjER li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeButton = $(this).attr('href');
+		$(activeButton).show();
+		return false;
+	});
+});
